b2b4bfb5ff0fedf53304e63760da62c1265d775e
mood7dev <mood7dev@gmail.com>
2025-05-22 18:04:02 +0900

    new file

--- src/ch02/sec02/CharExample.java
+++ src/ch02/sec02/CharExample.java
@@ -3,7 +3,7 @@ package ch02.sec02;
 public class CharExample {
     public static void main(String[] args) {
         char ch1 = 'A';
-        System.out.printf("ch1: %c, %d\n", ch1, (int)ch1);
+        System.out.printf("ch1: %c,` %d\n", ch1, (int)ch1);
         System.out.printf("66: %c, %d\n", 66, 66);
     }
 }
