08dc90a772c2639ab5617c188d61742fc0adf0dd
mood7dev <mood7dev@gmail.com>
2025-05-22 17:58:17 +0900

    new file

---
+++ src/ch02/sec02/IntegerLiteralExample.java
@@ -0,0 +1,12 @@
+package ch02.sec02;
+
+public class IntegerLiteralExample {
+    public static void main(String[] args) {
+        /*
+int var1 = 0b1011; // 2진수
+int var2 = 0206; // 8진수
+int var1 = 365; // 10진수
+int var1 = 0xB3; // 16진수 (0~9, A, B, C, D, E, F)
+         */
+    }
+}
