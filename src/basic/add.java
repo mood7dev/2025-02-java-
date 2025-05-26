4a743c7f91caea84b01b87c4c410081ddcbb14a3
mood7dev <mood7dev@gmail.com>
2025-05-26 18:04:23 +0900

    new file

--- src/basic/add.java
+++ src/basic/add.java
@@ -2,7 +2,7 @@ package basic;
 
 public class add {
     public static void main(String[] args) {
-String str = "5";
+
 
 
     }
---
+++ src/ch03/sec01/ArithmeticOperatorExample.java
@@ -0,0 +1,21 @@
+package ch03.sec01;
+
+public class ArithmeticOperatorExample {
+    public static void main(String[] args) {
+        //산술연산자
+        int v1 = 10, v2 = 4, v3 = 5;
+        long v4 = 10L;
+
+        int result1 = v1 + v2;
+        System.out.println("result1: " + result1);
+
+        long result2 = v1 + v2 - v4;
+        System.out.println("result2: " + result2);
+
+        double result3 = (double) v1 / v2;
+        System.out.println("result3: " + result3);
+
+        int result4 = v1 % v2;
+        System.out.println("result4: " + result4);
+    }
+}
