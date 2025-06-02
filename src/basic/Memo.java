fd9e34501b4fa4b7e24eeac166f56e325d212c4e
mood7dev <mood7dev@gmail.com>
2025-06-02 18:09:37 +0900

    Level up!

--- src/basic/Memo.java
+++ src/basic/Memo.java
@@ -1,20 +1,21 @@
 package basic;
 
-import java.util.Scanner;
-
 public class Memo {
     public static void main(String[] args) {
-        //Scanner.sc = new Scanner(System.in);
-    int st = 0;
-    int score = 0;
+        int result = add(10, 20);
+        System.out.println("result = " + result);
+    }
 
-        while (true) {
-            System.out.println("--------------------------------");
-            System.out.println("1. 학생수 | 2. 점수입력 | 3. 점수리스트 | 4. 분석 | 0. 종료");
-            System.out.println("--------------------------------");
-        }
+    public static int add(int x, int y) {
+        return x + y;
     }
 }
 
+/*
+(명사)속성, 필드, 전역변수
+(동사)동작, 메소드
 
+[ ]은 [ ]을 가지고 있다. has-a
+[ ]은 [ ]이다. is-a // 상속관계
 
+ */
---
+++ src/ch06/sec06/exam02/Car.java
@@ -0,0 +1,12 @@
+package ch06.sec06.exam02;
+
+public class Car {
+    boolean start;
+    String company;
+    String model = "그랜저";
+    int maxSpeed = 350;
+    int speed;
+
+    public Car() {
+    }
+}
